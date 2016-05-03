f2f582879a49ab1297fb6f72632ec58b47238ad9
yubin1030 <yubinpll9110@163.com>
2016-05-03 16:39:48 +0800

    更新注释

--- src/main/java/com/thinkgem/jeesite/modules/wx/dao/WxAccessLogDao.java
+++ src/main/java/com/thinkgem/jeesite/modules/wx/dao/WxAccessLogDao.java
@@ -6,11 +6,15 @@ package com.thinkgem.jeesite.modules.wx.dao;
 import com.thinkgem.jeesite.common.persistence.CrudDao;
 import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
 import com.thinkgem.jeesite.modules.wx.entity.WxAccessLog;
-
 /**
- * 微信访问日志DAO接口
+ * 
+ * <p>
+ * Description:微信访问日志DAO接口<br />
+ * </p>
+ * @title WxAccessLogDao.java
+ * @package com.thinkgem.jeesite.modules.wx.dao 
  * @author yubin
- * @version 2016-04-16
+ * @version 0.1 2016年5月3日
  */
 @MyBatisDao
 public interface WxAccessLogDao extends CrudDao<WxAccessLog> {
